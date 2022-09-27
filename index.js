4b79e348b4503e8f31f0ffc14d8d8ee92734dfc9
BenBonno <benoit.bonnoron@gmail.com>
2022-09-27 18:06:06 +0200

    test

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import chalk from 'chalk';
+console.log(chalk.blue('Hello npm!'));
 const userInfo = require("./userInformation");
 
 console.log(`Hi, my name is ${userInfo.userName}, i'm ${userInfo.age} old and `);
