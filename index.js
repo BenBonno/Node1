e0f3ac94a3b77baf95107e53c044eda59953b1cd
BenBonno <benoit.bonnoron@gmail.com>
2022-09-28 11:25:18 +0200

    test module chalk

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import chalk from 'chalk';
-console.log(chalk.blue('Hello npm!'));
+// import chalk from 'chalk';
+// console.log(chalk.blue('Now, just say Hello npm!'));
+
 const userInfo = require("./userInformation");
+// import packageMain from 'commonjs-package';
 
 console.log(`Hi, my name is ${userInfo.userName}, i'm ${userInfo.age} old and `);
 userInfo.displayPassions();
+
